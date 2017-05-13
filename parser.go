e9b8112f5b965f60d517ced91208156b1316bd8b
jspc <james@zero-internet.org.uk>
2017-05-13 13:06:07 +0100

    Flesh out config

--- parser.go
+++ parser.go
@@ -4,17 +4,21 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// Job provides a deserialized joburation document
+// Job provides a deserialized job document
 type Job struct {
 	// Name is the name of the job
 	Name string
 	// Env is a list of environment vars to apply
 	Env []string
-	// Args is a string containing arguments to the command
-	Args string
+	// Args is a list containing arguments to the command
+	Args []string
+	// Cron is a string representing a cron time string thing
+	Cron string
+	// Container is the container which is run
+	Container string
 }
 
-func Parse(f []byte) (c Job, err error) {
-	err = yaml.Unmarshal([]byte(f), &c)
+func Parse(f []byte) (j Job, err error) {
+	err = yaml.Unmarshal([]byte(f), &j)
 	return
 }
