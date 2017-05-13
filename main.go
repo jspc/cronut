e9bb198324d35b9df6b15ed9cebfe8e290d78a25
jspc <james@zero-internet.org.uk>
2017-05-13 09:18:04 +0100

    Initialise an SE4 instance

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"time"
+
+	"github.com/zeebox/go-http-middleware"
+	"github.com/zeebox/goose4"
+)
+
+var (
+	GitRepo = flag.String("repo", "", "Git repo which stores job configuration")
+)
+
+func main() {
+	flag.Parse()
+
+	c := goose4.Config{
+		ArtifactID:      "cronut",
+		BuildNumber:     "n/a",
+		BuildMachine:    "",
+		BuiltBy:         "",
+		BuiltWhen:       time.Now(),
+		CompilerVersion: "",
+		GitSha:          "",
+		RunbookURI:      "",
+		Version:         "v0.0.1",
+	}
+
+	se4, err := goose4.NewGoose4(c)
+	if err != nil {
+		panic(err)
+	}
+
+	http.Handle("/service/", middleware.NewMiddleware(se4))
+	panic(http.ListenAndServe(":8080", nil))
+}
